c9e641cb3600cd58f76a5c8c2e15b921eb569dbd
Prince Essel <prince.essel@publicissapient.com>
2019-08-13 11:40:09 -0500

    Adding the AppConstance file for this Project.

---
+++ app/src/main/java/com/example/mvvm_todo_app_impl/AppConstants.java
@@ -0,0 +1,8 @@
+package com.example.mvvm_todo_app_impl;
+
+public class AppConstants {
+    public static final int ADD_TASK_REQUEST_CODE = 1;
+    public static final int EDIT_TASK_REQUEST_CODE = 2;
+    public static final String EXTRA_TASK = "com.example.mvvm_todo_app_impl.TASK";
+    public static final String EXTRA_ID = "com.example.mvvm_todo_app_impl.EXTRA_ID";
+}
